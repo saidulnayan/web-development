45e1c4e69f0d86d3da39c2ef0b843c58be569607
Saidul Islam Nayan <55102265+saidulnayan@users.noreply.github.com>
2025-01-18 12:05:58 +0600

    Add files via upload

---
+++ College-Notes-Gallery/about.php
@@ -0,0 +1,64 @@
+<?php include 'includes/connection.php';?>
+<?php include 'includes/header.php';?>
+<?php include 'includes/navbar.php';?>
+
+<br><br>
+<link rel="stylesheet" type="text/css" href="styles.css" media="all" />
+    <link rel="stylesheet" type="text/css" href="demo.css" media="all" />
+    <!-- jQuery -->
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <!-- FlexSlider -->
+    <script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+    
+     <div class="slider_container"><br><br>
+     	<h1 style="font-size: 22px;">The College Notes Uploading System is a simple system that helps users to upload, download and manage notes of their particular course. Talking about the features of this system, it just contains both the admin section and the user section. The user can log in to look for various other notes/courses. While the admin can view all the users and manage them. The design of this project is pretty simple so that the user won’t find any difficulties while working on it. The admin can view and manage the users, view their uploads, and approve or delete their uploads.</h1>
+   	 </div>
+    </div>  
+</div>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'includes/footer.php';?>
+
+        
