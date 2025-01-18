f65fa3f36fb88bcbe97cbabe7e5d6afb62821cc4
Saidul Islam Nayan <55102265+saidulnayan@users.noreply.github.com>
2025-01-18 12:32:30 +0600

    Add files via upload

---
+++ das/patient/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>mms-patient</title>
+	<!-- First include jquery js -->
+<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<?php
+
+//if($_SESSION['donorstatus']!==""){
+	//header("location:../patient_login.php");
+//}
+
+?>
